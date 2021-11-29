9269898525cd9186fcc9dad65938664984c5e130
enochmaonga <enoch.mokua@student.moringaschool.com>
2021-11-29 23:26:49 +0300

    adding confitionals and variables

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+//user logic
 // Akan names are derived from Ghanian culture. Frequently in Ghana, children are given their first name as a 'day name' which corresponds to the day in the week they were born. Here are Ghanian day names.
 
 // Male
@@ -49,3 +50,24 @@
 var dayNames = ['Sunday', "Monday", "Tuesday", "Wednesday", "Thursday", 'Friday', 'Saturday'];
 var femaleNames = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
 var maleNames = ['Kwasi', 'Kwadwo','Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
+//
+
+var inPutValue = prompt('enter day of the month');
+
+if (dayNames == 1) {
+    alert("you were born on " + dayNames[0] + "and your akan Name is " + maleName[0] + "!");
+} else if(dayNames == 2) {
+    alert("you were born on" + dayNames[1] + "and your akan name is " + maleNames[1] + '!');
+}
+else if(dayNames == 3) {
+    alert("you were born on" + dayNames[2] + "and your akan name is " + maleNames[2] + '!');
+}
+else if(dayNames == 4) {
+    alert("you were born on" + dayNames[3] + "and your akan name is " + maleNames[3] + '!');
+}
+else if(dayNames == 5) {
+    alert("you were born on" + dayNames[4] + "and your akan name is " + maleNames[4] + '!');
+}
+else if(dayNames == 6) {
+    alert(`you were born on${dayNames[5]}and your akan name is ${maleNames[5]}!`);
+}
