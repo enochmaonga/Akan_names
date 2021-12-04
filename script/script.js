3ee2517de3a732fad847478be3ff8506c121ab09
enochmaonga <enoch.mokua@student.moringaschool.com>
2021-12-04 20:45:30 +0300

    adding more js arguments, html linking issues

--- script/script.js
+++ script/script.js
@@ -1,17 +1,39 @@
 //user logic or front end
  var day = document.getElementById("day").value;
- var day = parseInt(day);
+ var dd = parseInt(dd);
  var month = document.getElementById("month").value;
- var month = parseInt(month);
+ var mm = parseInt(mm);
  var year = document.getElementById("year").value;
- var year = parseInt(year);
+ var yy = parseInt(yy);
 
+ //formula
+ var result = parseInt(((CC / 4) - 2 * CC - 1) + ((5 * yy / 4)) + ((26 * (mm + 1) / 10)) + dd) % 7;
  
+ var maleNames = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
+ var femaleNames = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
+ var dayNames = ['Sunday', "Monday", "Tuesday", "Wednesday", "Thursday", 'Friday', 'Saturday'];
+if (document.getElementById("gender").checked) {
+    var gender = "male";
+}
+    else {
+        var gender = "female";
+        if(dd <= 0 || dd > 31) {
+            alert("invalid date");
+        }
+        else if(mm <= 0 || mm > 12) { 
+            alert("invalid month");
+        }
+        else if(mm == 2 && dd > 29) {
+            alert("invalid day of month");
+        }
+        else if (Math.round(result) == 0 && gender === 'male') {
+            document.getElementById("final").innerHTML = ("You were born on Sunday, your Akan Name is " + maleNames[0]);
+          }
 // Business (or back-end) logic:
      (Year, Month, Day, Gender) => {
-         var femaleNames = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
-         var maleNames = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
-         var dayNames = ['Sunday', "Monday", "Tuesday", "Wednesday", "Thursday", 'Friday', 'Saturday'];
+         
+         
+         
          var Day = new Date(Year, --Month, Day);
          if (Gender === 'Male') {
              return Day && maleNames[Day.getDay()];
@@ -23,26 +45,26 @@
 
 
 
-//  }
-// var inPutValue = prompt('enter day of the month');
+ }
+var inPutValue = prompt('enter day of the month');
 
-// if (dayNames == 1) {
-//     alert("you were born on " + dayNames[0] + "and your akan Name is " + maleName[0] + "!");
-// } else if(dayNames == 2) {
-//     alert("you were born on" + dayNames[1] + "and your akan name is " + maleNames[1] + '!');
-// }
-// else if(dayNames == 3) {
-//     alert("you were born on" + dayNames[2] + "and your akan name is " + maleNames[2] + '!');
-// }
-// else if(dayNames == 4) {
-//     alert("you were born on" + dayNames[3] + "and your akan name is " + maleNames[3] + '!');
-// }
-// else if(dayNames == 5) {
-//     alert("you were born on" + dayNames[4] + "and your akan name is " + maleNames[4] + '!');
-// }
-// else if(dayNames == 6) {
-//     alert(`you were born on${dayNames[5]}and your akan name is ${maleNames[5]}!`);
-// }
-// else if(dayNames == 7) {
-//     alert("you were born on" + dayNames[6] + "and your akan name is " + maleNames[6] + '!');
-// }
+if (dayNames == 1) {
+    alert("you were born on " + dayNames[0] + "and your akan Name is " + maleName[0] + "!");
+} else if(dayNames == 2) {
+    alert("you were born on" + dayNames[1] + "and your akan name is " + maleNames[1] + '!');
+}
+else if(dayNames == 3) {
+    alert("you were born on" + dayNames[2] + "and your akan name is " + maleNames[2] + '!');
+}
+else if(dayNames == 4) {
+    alert("you were born on" + dayNames[3] + "and your akan name is " + maleNames[3] + '!');
+}
+else if(dayNames == 5) {
+    alert("you were born on" + dayNames[4] + "and your akan name is " + maleNames[4] + '!');
+}
+else if(dayNames == 6) {
+    alert(`you were born on${dayNames[5]}and your akan name is ${maleNames[5]}!`);
+}
+else if(dayNames == 7) {
+    alert("you were born on" + dayNames[6] + "and your akan name is " + maleNames[6] + '!');
+}
