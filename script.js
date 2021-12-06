c53e1fd2adb65a3d1b1e9a29b521e91b668cf2d8
enochmaonga <enoch.mokua@student.moringaschool.com>
2021-12-06 21:45:18 +0300

    correcting errors on days entry

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
 //user logic or front end
 function submition() { 
+    /*declaration*/
  var day = document.getElementById("day").value;
- var dd = parseInt(dd);
+ var dd = parseInt(day);
  var month = document.getElementById("month").value;
- var mm = parseInt(mm);
+ var mm = parseInt(month);
  var year = document.getElementById("year").value;
  var yy = parseInt(year.toString().slice(2,4));
  var cc = Math.ceil(year / 100)
 
  // this is the formula we are given
- var result = parseInt(((CC / 4) - 2 * CC - 1) + ((5 * yy / 4)) + ((26 * (mm + 1) / 10)) + dd) % 7;
+ var result = parseInt(((cc / 4) - 2 * cc - 1) + ((5 * yy / 4)) + ((26 * (mm + 1) / 10)) + dd) % 7;
  
  var maleNames = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
  var femaleNames = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
 //  var dayNames = ['Sunday', "Monday", "Tuesday", "Wednesday", "Thursday", 'Friday', 'Saturday'];
 
 if (document.getElementById("gender").checked) {
-    var gender = "male";
+    var gender = 'male';
 }
 else {
-var gender = "female";
+var gender = 'female';
 }
 if(dd <= 0 || dd > 31) {
 alert("invalid date");
