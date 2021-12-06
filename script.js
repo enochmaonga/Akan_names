9275a7062b19e352b9492916de792e3e98b5a9c5
enochmaonga <enoch.mokua@student.moringaschool.com>
2021-12-06 22:24:41 +0300

    realining names and dates

--- script.js
+++ script.js
@@ -38,19 +38,19 @@ else if (Math.round(result) == 1 || Math.round(result) == -1 && gender === 'male
     document.getElementById("final").innerHTML = ("You were born on Monday, your Akan Name is " + maleNames[1]);
 }
 else if (Math.round(result) == 2 || Math.round(result) == -2 && gender === 'male') {
-    document.getElementById("final").innerHTML = ("You were born on Monday, your Akan Name is " + maleNames[2]);
+    document.getElementById("final").innerHTML = ("You were born on Tuesday, your Akan Name is " + maleNames[2]);
 }
 else if (Math.round(result) == 3 || Math.round(result) == -3 && gender === 'male') {
     document.getElementById("final").innerHTML = ("You were born on Wednesday, your Akan Name is " + maleNames[3]);
 }
 else if (Math.round(result) == 4 || Math.round(result) == -4 && gender === 'male') {
-    document.getElementById("final").innerHTML = ("You were born on Wednesday, your Akan Name is " + maleNames[4]);
+    document.getElementById("final").innerHTML = ("You were born on Thursday, your Akan Name is " + maleNames[4]);
 }
 else if (Math.round(result) == 5 || Math.round(result) == -5 && gender === 'male') {
-    document.getElementById("final").innerHTML = ("You were born on Wednesday, your Akan Name is " + maleNames[5]);
+    document.getElementById("final").innerHTML = ("You were born on Friday, your Akan Name is " + maleNames[5]);
 }
 else if (Math.round(result) == 6 || Math.round(result) == -6 && gender === 'male') {
-    document.getElementById("final").innerHTML = ("You were born on Wednesday, your Akan Name is " + maleNames[6]);
+    document.getElementById("final").innerHTML = ("You were born on Saturday, your Akan Name is " + maleNames[6]);
 }
 else if (Math.round(result) == 0 && gender === 'female') {
     document.getElementById("final").innerHTML = ("You were born on Sunday, your Akan Name is " + femaleNames[0]);
